1040a4d4ec70b0ca4932c4f0dffd5dfeb4468854
Yury Kurouski <ymorpheus@gmail.com>
2025-05-26 20:44:07 +0200

    Remove unused promptForFileConflict import from helpers

--- index.ts
+++ index.ts
@@ -8,7 +8,6 @@ import {
     revertOperations,
     promptUser,
     promptForDirectory,
-    promptForFileConflict,
     visualizeDirectoryStructure,
     generateOrganizationSummary
 } from './src/helpers';
